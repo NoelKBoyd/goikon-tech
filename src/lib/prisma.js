7a34598cfdd38b024c077ba200dae9147fb46e4f
Hafeez010 <c4052183@hallam.shu.ac.uk>
2025-03-13 09:53:56 +0000

    login improved

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,9 +1,5 @@
 import { PrismaClient } from '@prisma/client';
 
-<<<<<<< HEAD
-const prisma = new PrismaClient();
-export default prisma;
-=======
 const globalForPrisma = globalThis;
 
 const prisma = globalForPrisma.prisma || new PrismaClient();
@@ -11,4 +7,3 @@ const prisma = globalForPrisma.prisma || new PrismaClient();
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 export default prisma;
->>>>>>> fff3ca014e8cf2b926f07e021f148b4af1ea8f73
