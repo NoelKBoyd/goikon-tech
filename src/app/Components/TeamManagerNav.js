c846bc0159aa757b693d813fba18434f06e6f776
Alistair Craig <alistaircraig393@gmail.com>
2025-03-16 16:19:16 +0000

    Made the navbar component foe the team manager

---
+++ src/app/Components/TeamManagerNav.js
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import { VscAccount } from "react-icons/vsc";
+import { FcConferenceCall } from "react-icons/fc";
+
+export default function TopNav() {
+    return (
+      <div className="w-full h-20 bg-white-500 flex items-end text-xl m-0 pl-0">
+
+        <div className="h-full flex items-center pl-15">
+          <div>
+            <FcConferenceCall className="h-18 w-18"/>
+          </div>
+        </div>
+
+      <div className="flex justify-center items-center w-full">
+        <ul className="object-center flex space-x-8 border-b-1 border-gray-400">
+          <li className="inline hover:bg-gray-200 pb-3 pt-3"><a href="" className="px-6 py-3">Dashboard</a></li>
+          <li className="inline hover:bg-gray-200 pb-3 pt-3"><a href="" className="px-6 py-3">Team Roster</a></li>
+          <li className="inline hover:bg-gray-200 pb-3 pt-3"><a href="" className="px-6 py-3">Upcoming Matches</a></li>
+          <li className="inline hover:bg-gray-200 pb-3 pt-3"><a href="" className="px-6 py-3">Training Schedule</a></li>
+          <li className="inline hover:bg-gray-200 pb-3 pt-3"><a href="" className="px-6 py-3">Messages</a></li>
+          <li className="inline hover:bg-gray-200 pb-3 pt-3"><a href="" className="px-6 py-3">Settings</a></li>
+        </ul>
+      </div>
+
+        <div className="h-full flex items-center pr-15">
+          <div>
+            <VscAccount className="h-12 w-12 "/>
+          </div>
+        </div>
+
+      </div>
+    );
+  }
