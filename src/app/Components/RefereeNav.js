723f960f69785aea1687e17182088dbc96d4145e
Alistair Craig <alistaircraig393@gmail.com>
2025-03-16 16:27:13 +0000

    Made the navbar for the referee user

---
+++ src/app/Components/RefereeNav.js
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import { VscAccount } from "react-icons/vsc";
+import { FcConferenceCall } from "react-icons/fc";
+
+export default function TopNav() {
+    return (
+      <div className="w-full h-20 bg-white-500 flex items-end text-xl m-0 pl-0">
+
+        <div className="h-full flex items-center pl-15">
+          <div>
+            <FcConferenceCall className="h-18 w-18"/>
+          </div>
+        </div>
+
+      <div className="flex justify-center items-center w-full">
+        <ul className="object-center flex space-x-8 border-b-1 border-gray-400">
+          <li className="inline hover:bg-gray-200 pb-3 pt-3"><a href="" className="px-10 py-3">Dashboard</a></li>
+          <li className="inline hover:bg-gray-200 pb-3 pt-3"><a href="" className="px-10 py-3">Match Schedule</a></li>
+          <li className="inline hover:bg-gray-200 pb-3 pt-3"><a href="" className="px-10 py-3">Incident Reports</a></li>
+          <li className="inline hover:bg-gray-200 pb-3 pt-3"><a href="" className="px-10 py-3">Disciplinary Actions</a></li>
+          <li className="inline hover:bg-gray-200 pb-3 pt-3"><a href="" className="px-10 py-3">Settings</a></li>
+        </ul>
+      </div>
+
+        <div className="h-full flex items-center pr-15">
+          <div>
+            <VscAccount className="h-12 w-12 "/>
+          </div>
+        </div>
+
+      </div>
+    );
+  }
