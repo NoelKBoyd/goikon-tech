c2ceba3ae4486da201713b36eb189d2cc46ba676
NoelKBoyd <noel.boyd01@gmail.com>
2025-03-18 09:57:25 +0000

    Added Navbar and footers to all the pages from Hamad

--- src/app/fieldowner/page.js
+++ src/app/fieldowner/page.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import FieldOwnerNav from '../Components/FieldOwnerNav';
+import FieldOwnerSideBar from '../Components/FieldOwnerSideBar';
+import FieldOwnerFooter from '../Components/FieldOwnerFooter';
 
 const FieldOwnerPage = () => {
     return (
-        <FieldOwnerNav/>
+        <div>
+            <FieldOwnerNav/>
+            <FieldOwnerSideBar/>
+            <FieldOwnerFooter/>
+        </div>
     );
 };
 
