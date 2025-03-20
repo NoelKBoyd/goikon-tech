5d2535db138efabfcd69a6aff081bf48e70f59cb
Alistair <alistaircraig393@gmail.com>
2025-03-20 15:34:41 +0000

    made all of the links on the admin page take you to their corresponding pages

--- src/app/Components/AdminNav.js
+++ src/app/Components/AdminNav.js
@@ -16,12 +16,12 @@ export default function AdminNav() {
 
       <div className="flex justify-center items-center w-full">
         <ul className="object-center flex space-x-8">
-          <li className="inline hover:bg-gray-200 pb-3 pt-3"><a href="" className="px-10 py-3">Dashboard</a></li>
-          <li className="inline hover:bg-gray-200 pb-3 pt-3"><a href="" className="px-10 py-3">User Managment</a></li>
+          <li className="inline hover:bg-gray-200 pb-3 pt-3"><a href="/admin/Dashboard" className="px-10 py-3">Dashboard</a></li>
+          <li className="inline hover:bg-gray-200 pb-3 pt-3"><a href="/admin/UserManagment" className="px-10 py-3">User Managment</a></li>
           <li className="inline hover:bg-gray-200 pb-3 pt-3"><a href="/admin/TeamsPage" className="px-10 py-3">Teams</a></li>
-          <li className="inline hover:bg-gray-200 pb-3 pt-3"><a href="" className="px-10 py-3">Matches</a></li>
-          <li className="inline hover:bg-gray-200 pb-3 pt-3"><a href="" className="px-10 py-3">Reports</a></li>
-          <li className="inline hover:bg-gray-200 pb-3 pt-3"><a href="" className="px-10 py-3">Settings</a></li>
+          <li className="inline hover:bg-gray-200 pb-3 pt-3"><a href="/admin/Matches" className="px-10 py-3">Matches</a></li>
+          <li className="inline hover:bg-gray-200 pb-3 pt-3"><a href="/admin/Reports" className="px-10 py-3">Reports</a></li>
+          <li className="inline hover:bg-gray-200 pb-3 pt-3"><a href="/admin/SettingsPage" className="px-10 py-3">Settings</a></li>
         </ul>
       </div>
 
--- src/app/admin/SettingsPage/page.js
+++ src/app/admin/SettingsPage/page.js
@@ -4,7 +4,7 @@ import AdminSideBar from "@/app/Components/AdminSideBar";
 import AdminFooter from "@/app/Components/AdminFooter";
 import { useState } from 'react';
 
-const TeamPage = () => {
+const Settings = () => {
     return (
         <div>
             <header>
@@ -16,7 +16,7 @@ const TeamPage = () => {
 
                 <div className='col-start-2 col-end-3 flex justify-center text-center'>
                     <div className="pt-10">
-                        <h1 className="text-xl"><strong>Teams</strong></h1>
+                        <h1 className="text-xl"><strong>Settings</strong></h1>
                         
                     </div>
                 </div>
@@ -29,4 +29,4 @@ const TeamPage = () => {
     );
 };
 
-export default TeamPage;
+export default Settings;
