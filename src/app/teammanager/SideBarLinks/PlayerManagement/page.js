5f859931f46060e9813f0ca5a292632186923e57
AA-program <aarizviuk@hotmail.com>
2025-04-01 10:44:22 +0100

    Player stats table

---
+++ src/app/teammanager/SideBarLinks/PlayerManagement/page.js
@@ -0,0 +1,30 @@
+'use client';
+import TeamManagerNav from '@/app/Components/TeamManagerNav';
+import TeamManagerSideBar from '@/app/Components/TeamManagerSideBar';
+import TeamManagerFooter from '@/app/Components/TeamManagerFooter';
+import { useState } from 'react';
+import BasicTable from '@/app/Components/PlayerStats';
+
+const PlayerStats = () => {
+    return (
+        <div>
+            <header>
+                <TeamManagerNav/>
+            </header>
+
+            <main className='grid w-full grid-cols-[260px_auto] bg-gray-100 h-screen'>
+                <TeamManagerSideBar className='col-start-1 col-end-2'/>
+
+                <div className= 'col-start-2 col-end-3 pt-10'>
+                    <BasicTable></BasicTable>
+                </div>
+            </main>
+
+            <footer>
+                <TeamManagerFooter/>
+            </footer>
+        </div>
+    );
+};
+
+export default PlayerStats;
