c8bcc098a664ce007aae6834fd2433f2862230b7
Hafeez010 <c4052183@hallam.shu.ac.uk>
2025-04-10 10:45:30 +0100

    homepage

--- src/app/teammanager/page.js
+++ src/app/teammanager/page.js
@@ -11,46 +11,46 @@ import PlayerStats from '../Components/PlayerStats';
 
 const TeamManagerPage = () => {
     return (
-        <div className="flex flex-col h-screen">
+        <div className="flex flex-col min-h-screen">
             <header>
                 <TeamManagerNav />
             </header>
 
-            <main className="flex-grow bg-gray-100">
-                <div className="flex">
-                    <div className="w-64">
-                        <TeamManagerSideBar />
-                    </div>
+            <main className="flex flex-grow bg-gray-100 overflow-y-auto">
+                <div className="w-64 flex-shrink-0">
+                    <TeamManagerSideBar />
+                </div>
 
-                    <div className="flex-1 grid grid-cols-3 grid-rows-2 gap-4 p-4">
+                <div className="flex-1 p-4">
+                    <div className="grid grid-cols-3 grid-rows-[auto,auto] gap-4 items-start">
                         <div className="row-start-1 col-start-1">
                             <TeamStats />
                         </div>
 
-                        <div className="row-start-1 col-start-2 justify-center">
+                        <div className="row-start-1 col-start-2">
                             <UpcomingMatch />
+                        </div>
+
+                        <div className="row-start-1 col-start-3">
+                            <NestedModal />
                         </div>
 
                         <div className="row-start-2 col-start-1">
                             <Calendar />
                         </div>
 
-                        <div className="row-start-2 col-start-3 flex justify-end pb-15">
-                            <MessagingPanel />
+                        <div className="row-start-2 col-start-2 max-h-[350px] overflow-auto">
+                            <PlayerStats />
                         </div>
 
-                        {/* Future placeholders */}
-                        <div className="row-start-1 col-start-3">
-                            <NestedModal />
-                        </div>
-                        <div className="row-start-2 col-start-2">
-                            <PlayerStats />
+                        <div className="row-start-2 col-start-3 pb-15">
+                            <MessagingPanel />
                         </div>
                     </div>
                 </div>
             </main>
 
-            <footer className="mt-auto">
+            <footer className="bg-white shadow">
                 <TeamManagerFooter />
             </footer>
         </div>
